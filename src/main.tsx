f29ebcd087314ef9426edc30d21e35d5487f1b9b
bobo88882005 <bobo88882005@yahoo.it>
2026-08-02 22:46:34 +0200

    Refactor ReactDOM rendering syntax

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,34 @@
 import React from "react";
+
+
 import ReactDOM from "react-dom/client";
 
+
 import App from "./App";
+
 
 import "./index.css";
 
 
-ReactDOM
-  .createRoot(
-    document.getElementById("root")!
-  )
-  .render(
 
-    <React.StrictMode>
 
-      <App />
 
-    </React.StrictMode>
 
-  );
+
+ReactDOM.createRoot(
+
+  document.getElementById("root")!
+
+)
+
+.render(
+
+
+  <React.StrictMode>
+
+    <App />
+
+  </React.StrictMode>
+
+
+);
