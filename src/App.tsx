dd746ac3f46b11b5f0df7e5af4c423a05609fd67
bobo88882005 <bobo88882005@yahoo.it>
2026-08-02 21:43:41 +0200

    Refactor App component to use Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
+import Home from "./pages/Home";
+
+
 export default function App(){
+
 
   return (
 
-    <div className="
-      min-h-screen
-      bg-black
-      text-white
-      flex
-      items-center
-      justify-center
-    ">
-
-      Insights Pro
-
-    </div>
+    <Home />
 
   );
 
