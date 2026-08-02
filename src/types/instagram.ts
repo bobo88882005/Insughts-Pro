e5c01666686047a3e1942db1f521b6419de87f17
bobo88882005 <bobo88882005@yahoo.it>
2026-08-02 21:40:11 +0200

    Update instagram.ts

--- src/types/instagram.ts
+++ src/types/instagram.ts
@@ -7,3 +7,62 @@ export interface InstagramUser {
   followedAt?: Date;
 
 }
+
+
+
+export interface ParsedInstagramData {
+
+  followers: InstagramUser[];
+
+  following: InstagramUser[];
+
+  pendingRequests: InstagramUser[];
+
+  receivedRequests: InstagramUser[];
+
+  recentlyUnfollowed: InstagramUser[];
+
+}
+
+
+
+export interface InstagramAnalysis {
+
+  followers: InstagramUser[];
+
+  following: InstagramUser[];
+
+  notFollowingBack: InstagramUser[];
+
+  youDontFollowBack: InstagramUser[];
+
+  reciprocal: InstagramUser[];
+
+  pendingRequests: InstagramUser[];
+
+  receivedRequests: InstagramUser[];
+
+  recentlyUnfollowed: InstagramUser[];
+
+  possibleInactive: InstagramUser[];
+
+  excludedUsers: InstagramUser[];
+
+
+  followersCount: number;
+
+  followingCount: number;
+
+  originalFollowingCount: number;
+
+  excludedCount: number;
+
+  inactiveCount: number;
+
+  reciprocalCount: number;
+
+  notFollowingBackCount: number;
+
+  youDontFollowBackCount: number;
+
+}
