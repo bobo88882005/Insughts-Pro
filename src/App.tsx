62056c045efb1da57e299f7bd33273898c52d386
bobo88882005 <bobo88882005@yahoo.it>
2026-08-02 19:47:46 +0200

    Create initial App component with basic layout

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+export default function App(){
+
+  return (
+
+    <div className="
+      min-h-screen
+      bg-black
+      text-white
+      flex
+      items-center
+      justify-center
+    ">
+
+      Insights Pro
+
+    </div>
+
+  );
+
+}
